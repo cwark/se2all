fec1eca290f3cd7722906178fe9fe0b6a89e6544
Alexey Khodyrev <ahodyrev@mail.ru>
2019-12-02 23:20:09 +0300

    add testLeftMenuItems

--- sel2all/src/test/java/ru/cwark/selenium/LitecartTests.java
+++ sel2all/src/test/java/ru/cwark/selenium/LitecartTests.java
@@ -2,13 +2,20 @@ package ru.cwark.selenium;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.System.out;
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
 
-public class LitecartTests extends TestBase{
+public class LitecartTests extends TestBase {
 
     private final String baseUrl = "http://localhost/litecart/";
     private final String adminPage = baseUrl + "admin/";
+
 
     @Test
     public void openAdminPageTest() {
@@ -17,6 +24,44 @@ public class LitecartTests extends TestBase{
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
         wait.until(titleIs("Cwark Store"));
+
     }
 
+    @Test
+    public void testLeftMenuItems() {
+        driver.get(adminPage);
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+        wait.until(titleIs("Cwark Store"));
+
+        List<WebElement> lis = driver.findElements(By.xpath("//li[@id='app-']/a/span[@class='name']"));
+        out.println(lis.size());
+        List<String> liNames = new ArrayList<String>();
+        for (WebElement li : lis) {
+            out.println(li.getText().trim());
+            liNames.add(li.getText().trim());
+        }
+
+        for (String liName : liNames) {
+            WebElement li = driver.findElement(By.xpath("//span[text()='" + liName + "']/.."));
+            li.click();
+            wait.until(visibilityOfElementLocated(By.cssSelector("h1")));
+            List<WebElement> uls = driver.findElements(By.xpath("//ul[@class='docs']//span[@class='name']"));
+            out.println(liName + ":" + uls.size());
+            if (uls.size() > 0) {
+                List<String> ulNames = new ArrayList<String>();
+                for (WebElement ul : uls) {
+                    out.println(ul.getText().trim());
+                    ulNames.add(ul.getText().trim());
+                }
+                for (String ulName : ulNames) {
+                    WebElement ul = driver.findElement(By.xpath("//ul[@class='docs']//span[text()='" + ulName + "']/.."));
+                    ul.click();
+                    wait.until(visibilityOfElementLocated(By.cssSelector("h1")));
+                }
+            }
+        }
+
+    }
 }
--- sel2all/src/test/java/ru/cwark/selenium/TestBase.java
+++ sel2all/src/test/java/ru/cwark/selenium/TestBase.java
@@ -2,6 +2,7 @@ package ru.cwark.selenium;
 
 import org.junit.After;
 import org.junit.Before;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -9,6 +10,10 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class TestBase {
     public WebDriver driver;
     public WebDriverWait wait;
+
+    public boolean areElementsPresent(By locator) {
+        return driver.findElements(locator).size() > 0;
+    }
 
     @Before
     public void start() {
